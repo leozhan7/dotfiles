a0b2a107d305f94cf1c7b12b5a3e675a9004cb39
LeoZhan <leozhan@kkbox.com>
2018-03-01 16:43:23 +0800

    Create .bashrc

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ ln -s "`pwd`/.bash_profile" ~/.bash_profile
 ln -s "`pwd`/.gitconfig" ~/.gitconfig
 ln -s "`pwd`/.vim" ~/.vim
 ln -s "`pwd`/.vimrc" ~/.vimrc
+ln -s "`pwd`/.bashrc" ~/.bashrc
 source ~/.bash_profile
+source ~/.bashrc
